783a102d79d10711d5c984329c1346dae3d6f9e1
alesdev <aleswebgit@gmail.com>
2023-10-07 10:34:38 +0200

    🚧 temporal setCurrentMusic on PlayButtonCard

--- src/components/PlayButtonCard.tsx
+++ src/components/PlayButtonCard.tsx
@@ -1,4 +1,5 @@
 import { Pause, Play } from "@icons"
+import { playlists } from "@lib/data";
 import { usePlayerStore } from "@store/playerStore";
 
 interface PlayButtonCardProps {
@@ -11,12 +12,26 @@ export function PlayButtonCard ({
   const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state)
 
   const handlePlay = () => {
+    setCurrentMusic({
+      playlist: {
+        id: playlistId,
+        albumId: Number(playlistId),
+        title: playlists[Number(playlistId)].title,
+        color: playlists[Number(playlistId)].color,
+        cover: playlists[Number(playlistId)].cover,
+        artists: playlists[Number(playlistId)].artists,
+      },
+      song: null,
+      songs: [],
+    })
     setIsPlaying(!isPlaying)
   }
 
+  const isPlayingPlaylist = currentMusic?.playlist?.id === playlistId
+
   return (
     <button onClick={handlePlay} className="card-play-button rounded-full bg-green-500 p-4">
-      {isPlaying ? <Pause /> : <Play />}
+      {isPlayingPlaylist ? <Pause /> : <Play />}
     </button>
   )
 }
