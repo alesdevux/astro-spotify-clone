9a7e4e4080a1e3d658951f470de6a2d7adf877d7
alesdev <aleswebgit@gmail.com>
2023-10-07 06:52:21 +0200

    ♻️ useGreeting hook

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,12 +1,7 @@
+import { useGreeting } from "@hooks/useGreeting";
+
 export function Greeting (): JSX.Element {
-  const currentTime = new Date()
-  const currentHour = currentTime.getHours()
-
-  let greeting = ''
-
-  if (currentHour < 12) greeting = 'Good morning'
-  else if (currentHour < 18) greeting = 'Good afternoon'
-  else greeting = 'Good evening'
+  const { greeting } = useGreeting()
   
   return (
     <h3 className="text-3xl pt-10 pb-6 font-bold">{greeting}</h3>
