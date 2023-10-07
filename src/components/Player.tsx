4acf02dc35b1162695f4a6ebfbc65ca3b7fb39c7
alesdev <aleswebgit@gmail.com>
2023-10-07 07:21:01 +0200

    🌱 start Player React component

---
+++ src/components/Player.tsx
@@ -0,0 +1,24 @@
+import { Pause, Play } from "@icons"
+import { useState } from "react"
+
+export function Player (): JSX.Element {
+  const [isPlaying, setIsPlaying] = useState<boolean>(false)
+
+  return (
+    <div className="flex items-center justify-between w-full px-4 z-50">
+      <section>
+        <h3 className="text-xl pt-10 pb-6 font-bold">Current Song...</h3>
+      </section>
+      <section className="grid place-content-center gap-4 flex-1">
+        <div className="flex justify-center">
+          <button className="bg-white rounded-full p-2 text-zinc-900" onClick={() => setIsPlaying(!isPlaying)}>
+            {isPlaying ? <Pause /> : <Play />}
+          </button>
+        </div>
+      </section>
+      <section>
+        <h3 className="text-xl pt-10 pb-6 font-bold">Volume</h3>
+      </section>
+    </div>
+  )
+}
