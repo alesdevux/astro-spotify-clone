ff3907cba02046277b1b65d320f52298e516095e
alesdev <aleswebgit@gmail.com>
2023-10-07 08:44:24 +0200

    ✨ Player works

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,8 +1,26 @@
 import { Pause, Play } from "@icons"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export function Player (): JSX.Element {
   const [isPlaying, setIsPlaying] = useState<boolean>(false)
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  useEffect(() => {
+    if (!audioRef.current) return
+
+    audioRef.current.src = `/music/1/01.mp3`
+  }, [])
+
+  const handlePlay = () => {
+    if (!audioRef.current) return
+
+    if (isPlaying) audioRef.current.pause()
+    if (!isPlaying) {
+      audioRef.current.volume = 0.5
+      audioRef.current.play()
+    }
+    setIsPlaying(!isPlaying)
+  }
 
   return (
     <div className="flex items-center justify-between w-full px-4 z-50">
@@ -11,14 +29,16 @@ export function Player (): JSX.Element {
       </section>
       <section className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center">
-          <button className="bg-white rounded-full p-3 text-zinc-900" onClick={() => setIsPlaying(!isPlaying)}>
+          <button className="bg-white rounded-full p-3 text-zinc-900" onClick={handlePlay}>
             {isPlaying ? <Pause /> : <Play />}
           </button>
         </div>
       </section>
-      <section>
+      <section className="grid place-content-center">
         <h3 className="text-xl pt-10 pb-6 font-bold">Volume</h3>
       </section>
+
+      <audio ref={audioRef} />
     </div>
   )
 }
